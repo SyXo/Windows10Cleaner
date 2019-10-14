801f7f9c148aa3f7d5d036a577548560272988ec
Rodolphe Colleie <rodolphe.colleie@epitech.eu>
2019-10-14 09:52:10 +0700

    DisableUnwantedServices: Disabling even more services, thanks to dev0x0 and FR33THY's script

--- DisableUnwantedServices.ps1
+++ DisableUnwantedServices.ps1
@@ -4,20 +4,48 @@ $ErrorActionPreference = "SilentlyContinue"
 Function DisableUnwantedServices
 {
 	$services = @(
+		"AJRouter"					# AllJoyn Router Service
+		"AppXSvc"					# AppX Deployment Service
+		"ALG"						# Application Layer Gateway Service
+		"AppMgmt"					# Application Management
+		"AppReadiness"					# Disabling App Readiness
+		"AssignedAccessManagerSvc"			# AssignedAccessManager
 		"BcastDVRUserService"
+		"BDESVC"					# BitLocker Drive Encryption Service
+		"BITS"						# Background Intelligent Transfer Service
+		#"BTAGService"					# Bluetooth Audio Gateway Service
+		#"bthserv"					# Bluetooth Support Service
+		#"BthHFSrv"					# Bluetooth Handsfree Service
+		"CDPSvc"					# Connected Devices Platform Service
+		"CertPropSvc"					# Certificate Propagation
+		"ClipSVC"					# Client License Service (ClipSVC)
 		"diagnosticshub.standardcollector.service"	# Microsoft® Diagnostics Hub Standard Collector Service
-		"diagsvc"
+		"diagsvc"					# Diagnostic Execution Service
 		"DiagTrack"					# Diagnostics Tracking Service
-		"diagsvc"
+		"DisplayEnhancementService"
 		"dmwappushservice"				# WAP Push Message Routing Service
+		"DoSvc"						# Delivery Optimisation
+		"DPS"						# Diagnostic Policy Service
+		"DusmSvc"					# Data Usage
+		"EntAppSvc"					# Enterprise App Management Service
+		"EFS"						# Encrypting File System
+		"fdPHost"					# Function Discovery Provider Host
+		"fhsvc"						# File History Service
+		"FDResPub"					# Function Discovery Resource Publication
+		"GraphicsPerfSvc"				# GraphicsPerfSvc
+		"hns"						# Host Network Service
 		"HomeGroupListener"				# HomeGroup Listener
 		"HomeGroupProvider"				# HomeGroup Provider
+		"HvHost"					# HV Host Service
+		"KeyIso"					# CNG Key Isolation
 		"lfsvc"						# Geolocation Service
 		"MapsBroker"					# Downloaded Maps Manager
 		"MessagingService"
+		"MSDTC"						# Distributed Transaction Coordinator
 		"ndu"						# Windows Network Data Usage Monitor
 		"NetTcpPortSharing"				# Net.Tcp Port Sharing Service
 		"OneSyncSvc"
+		"PeerDistSvc"					# BranchCache
 		"PimIndexMaintenanceSvc"
 		"PushToInstall"
 		# "PcaSvc"					# Program compatibility assistant
@@ -29,14 +57,23 @@ Function DisableUnwantedServices
 		"SharedAccess"					# Internet Connection Sharing (ICS)
 		"SessionEnv"
 		# "shpamsvc"
+		"Superfetch"					# Versions older than 1903
 		"SysMain"					# Superfetch's name on 1903+
 		"TermService"
 		"TrkWks"					# Distributed Link Tracking Client
 		"TroubleshootingSvc"
+		"tzautoupdate"					# Auto Time Zone Updater
 		"UmRdpService"
 		"UnistoreSvc"
 		"UserDataSvc"
+		"VaultSvc"					# Credential Manager
+		"vmickvpexchange"				# Hyper-V Data Exchange Service
+		"vmicguestinterface"				# Hyper-V Guest Service Interface
+		"vmicshutdown"					# Hyper-V Guest Shutdown Service
+		"wbengine"					# Block Level Backup Engine Service
 		"WbioSrvc"					# Windows Biometric Service (required for Fingerprint reader / facial detection)
+		"WdiServiceHost"				# Diagnostic Service Host
+		"WdiSystemHost"					# Diagnostic System Host
 		"wercplsupport"					# Problem report
 		"WerSvc"					# Windows report
 		# "WlanSvc"					# WLAN AutoConfig (WiFi Networks)
